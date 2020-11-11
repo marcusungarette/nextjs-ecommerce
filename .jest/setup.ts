505e5e711e12a6ea72faaf3324bc77807fb94f0a
marcusungarette <marcusungarette@gmail.com>
2020-11-11 17:27:48 -0300

    Jest configuration

---
+++ .jest/setup.ts
@@ -0,0 +1,1 @@
+// Qualquer coisa q eu precise importar para o Jest
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+// Todas as configs do Jest aqui, deixa meu package.json mais limpo
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
