df24acddc3bc0b5a90b9f5ab080346e21f9adbee
marcusungarette <marcusungarette@gmail.com>
2020-11-11 16:23:58 -0300

    Prettier/Eslint configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+type Props = {
+  title: string
+}
+export default function Home({ title = 'React'}: Props) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+    </div>
+  )
+}
