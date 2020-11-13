b75d6e176a7e5b18305f4315b7fdf634463f4dd1
marcusungarette <marcusungarette@gmail.com>
2020-11-13 15:38:09 -0300

    Added images, styled Main component with test coverage

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,20 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avancado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um Atomo e React Avancado escrito ao lado"
+    />
+    <S.Title>React Avancado</S.Title>
+    <S.Description>
+      Typescript, ReactJS NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Desenvolvedor de frente para a tela com codigo"
+    />
+  </S.Wrapper>
 )
 
 export default Main
