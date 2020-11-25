9f7a974a952b8744d820077ba66b775123f70464
marcusungarette <marcusungarette@gmail.com>
2020-11-25 17:11:12 -0300

    Added Generators to components

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-// Todas as configs do Jest aqui, deixa meu package.json mais limpo
 module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }
